3ba2f7db070a7fc858ce0ae4fc344cebdbe53c64
香榭的落叶 <45730093+1298509345@users.noreply.github.com>
2025-01-05 20:06:47 +0800

    fix: build.sh 语法错误

--- build.sh
+++ build.sh
@@ -164,7 +164,7 @@ for server in "${remote_servers[@]}"; do
       echo "镜像: \$img"
       tag=\$(echo "\$img" | awk '{print \$2}')
       echo "镜像tag: \$tag, 构建tag: $docker_image_tag"
-      if [ "\$tag"!="$docker_image_tag" ]; then
+      if [ "\$tag" != "$docker_image_tag" ]; then
         img_id=\$(echo "\$img" | awk '{print \$3}')
         docker rmi \$img_id
       fi
