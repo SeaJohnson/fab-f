dfcdce26ed2af1878eedfbb4d4d9d012a4d1d901
zhangzhiwei <2318924118@qq.com>
2025-01-09 21:32:36 +0800

    feat: 添加网络请求

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,6 +6,44 @@
  * @Description:
  * Copyright (c) 2024 by 朗新科技, All Rights Reserved.
  */
+import { useEffect } from "react";
+
 export default () => {
+
+  useEffect(() => {
+    getList()
+  }, [])
+  
+  const getList = () => {
+    fetch('http://fab-dev:9002/health')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('发生错误:', error);
+  });
+  }
+
+  const getList2 = () => {
+    fetch('https://api.example.com/data', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        key: 'value'
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('发生错误:', error);
+      });
+    
+  }
+
   return <div>Hello</div>;
 };
